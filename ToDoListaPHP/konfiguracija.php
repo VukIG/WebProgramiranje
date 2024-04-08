ab7a2e8cd455bcc9a7b854e40df4d2cef98a2812
VukIG <111462330+VukIG@users.noreply.github.com>
2024-04-08 23:35:17 +0200

    Update konfiguracija.php

--- ToDoListaPHP/konfiguracija.php
+++ ToDoListaPHP/konfiguracija.php
@@ -1,8 +1,8 @@
 <?php
 
 	define('DB_SERVER', '127.0.0.1');
-	define('DB_USERNAME', 'admir');
-	define('DB_PASSWORD', 'R2d2jedijeti.');
+	define('DB_USERNAME', 'root');
+	define('DB_PASSWORD', '');
 	define('DB_NAME', 'todolista');
 	
 	$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
